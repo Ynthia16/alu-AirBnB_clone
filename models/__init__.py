b9d1a2600e297116f1a562121a22138d81b5aa50
ynthia16 <c.umugwanez@alustudent.com>
2023-09-11 02:32:59 -0700

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""The __init__ method for models directory"""
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
-from models.amenity import Amenity
-from models.city import City
-from models.review import Review
 from models.state import State
-from models.engine import file_storage
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
+
+classes = {"BaseModel": BaseModel, "User": User,  "State": State,
+           "City": City, "Amenity": Amenity, "Place": Place,
+           "Review": Review}
--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,55 @@
 #!/usr/bin/python3
-""" Module for Base """
-
+"""
+This module defines a base class for
+all models in our hbnb clone
+"""
+import uuid
 from datetime import datetime
-from uuid import uuid4
-import json
 import models
-
-format_date = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """ Basemodel class """
+    """
+    BaseModel that defines all common
+    attributes/methods for other classe
+    """
 
     def __init__(self, *args, **kwargs):
-        """ Initialization of Database """
-        if args is not None and len(args) > 0:
-            pass
+        """
+        Constructor for BaseModel
+        """
         if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_date)
-                if key not in ['__class__']:
-                    setattr(self, key, item)
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
-    def __str__(self):
-        """str definition"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
     def save(self):
-        """save definition"""
+        """
+            updates the public instance attribute
+            updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
+        """
+            returns a dictionary containing all
+            keys/values of __dict__ of the instance
+        """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+
+    def __str__(self):
+        """Representation of BaseModel instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
